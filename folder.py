2de818f0937a66b97e85fd5e73145f67c3fa28ee
alloy01 <ajeettsingh51@gmail.com>
2026-09-04 22:25:33 +0530

    intialized the sift repository

---
+++ folder.py
@@ -0,0 +1,71 @@
+document_extensions = [
+    ".pdf",
+    ".doc",
+    ".docx",
+    ".txt",
+    ".rtf",
+    ".odt",
+    ".xls",
+    ".xlsx",
+    ".csv",
+    ".ppt",
+    ".pptx",
+    ".odp",
+    ".md"
+]
+
+image_extensions = [
+    ".jpg",
+    ".jpeg",
+    ".png",
+    ".gif",
+    ".bmp",
+    ".webp",
+    ".tiff",
+    ".tif",
+    ".svg",
+    ".ico",
+]
+
+program_extensions = [
+    ".exe",
+    ".msi",
+    ".bat",
+    ".cmd",
+    ".com",
+    ".scr",
+    ".msix"
+]
+
+archive_extensions = [
+    ".zip",
+    ".rar",
+    ".7z",
+    ".tar",
+    ".gz",
+    ".bz2",
+    ".xz",
+    ".iso",
+]
+
+audio_extensions = [
+    ".mp3",
+    ".wav",
+    ".flac",
+    ".aac",
+    ".m4a",
+    ".ogg",
+    ".opus",
+    ".wma",
+]
+
+video_extensions = [
+    ".mp4",
+    ".mkv",
+    ".avi",
+    ".mov",
+    ".wmv",
+    ".flv",
+    ".webm",
+    ".m4v",
+]
---
+++ main.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+import shutil
+from folder import document_extensions, image_extensions, program_extensions, audio_extensions, video_extensions, archive_extensions
+
+path = Path("../../Downloads")
+
+required_folders = [
+    "Documents",
+    "Images",
+    "Programs",
+    "Other",
+    "Videos",
+    "Audio",
+    "Archives",
+]
+
+for folder_name in required_folders:
+    folder_name = path / folder_name
+    folder_name.mkdir(exist_ok= True)
+
+def move_file(item, folder):
+    shutil.move(item, path / folder)
+    print(f"moved {item} --> {folder}")
+
+for item in path.iterdir():
+
+    if item.is_file():
+
+        extension = item.suffix.lower()
+        
+        if extension in document_extensions:
+            move_file(item, "Documents")
+        elif extension in image_extensions:
+            move_file(item, "Images")
+        elif extension in program_extensions:
+            move_file(item, "Programs")
+        elif extension in archive_extensions:
+            move_file(item, "Archives")
+        elif extension in video_extensions:
+            move_file(item, "Videos")
+        elif extension in audio_extensions:
+            move_file(item, "Audio")
+        else:
+            move_file(item, "Other")
+
+print("\n Sorting complete")
