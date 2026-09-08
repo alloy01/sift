bbbce45ce2a2a3a063922ed1adafd72038117506
alloy01 <ajeettsingh51@gmail.com>
2026-09-08 22:54:44 +0530

    feat: removed the hardcoded path directing to downloads folder, desired path can be entered now if program to be exited press E.

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from pathlib import Path
 import shutil
+import sys
 from folder import document_extensions, image_extensions, program_extensions, audio_extensions, video_extensions, archive_extensions, required_folders, categories
 
-path = Path("../../Downloads")
+desire = input("\nEnter relative path of the directory (E to Exit): ")
+
+if(desire == "E"):
+    print("Program exited successfully\n")
+    sys.exit(0)
+
+path = Path(desire)
 
 for folder_name in required_folders:
     folder_name = path / folder_name
@@ -32,4 +39,4 @@ for item in path.iterdir():
 GREEN = "\033[32m"
 RESET = "\033[0m"
 
-print(f"{GREEN}\n✓ Sorting completed {RESET}")
+print(f"{GREEN}\n✓ Sorting completed {RESET}\n")
